e31dee2a56ab1727f04f724ae77c863167c39f5c
Izhar Firdaus <izhar@inigo-tech.com>
2013-06-02 22:55:02 +0800

    Preparing release 1.13

--- CHANGES.txt
+++ CHANGES.txt
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-1.13 (unreleased)
+1.13 (2013-06-02)
 -----------------
 
 - Nothing changed yet.
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.13.dev0'
+version = '1.13'
 
 long_description = (
     open('README.rst').read()
